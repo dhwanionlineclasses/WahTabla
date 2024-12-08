a0483e99f1a1b30ce42a9806b01757486ef36da3
Kaialan <satyajit1501@gmail.com>
2024-12-08 18:47:04 +0530

    fix: ip fetching

--- pages/api/location.ts
+++ pages/api/location.ts
@@ -4,7 +4,15 @@ const IpInfoApiToken = process.env.IPINFO_API_TOKEN;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const response = await fetch(`https://ipinfo.io/json?token=${IpInfoApiToken}`);
+    // const response = await fetch(`https://ipinfo.io/json?token=${IpInfoApiToken}`);
+
+    // Fetch the user's IP address from request headers
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+    // console.log('Client IP:', ip);
+
+    // Fetch location data from ipinfo.io
+    const response = await fetch(`https://ipinfo.io/${ip}/json?token=${IpInfoApiToken}`);
 
     if (!response.ok) {
       console.error('Failed to fetch location:', response.status, response.statusText);
@@ -13,7 +21,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     
     const data = await response.json();
-    console.log('Fetched location data:', data);
+    // console.log('Fetched location data:', data);
 
     res.status(200).json({ country: data.country, region: data.region });
   } catch (error) {
